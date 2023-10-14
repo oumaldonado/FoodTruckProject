ff98d87444eee1684ee9570cd4343dbe695ca6bf
Osvaldo Maldonado <osvaldomaldonado404@gmail.com>
2023-10-14 15:44:33 -0400

    Foodtrucks without Readme

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,19 +1,111 @@
 package com.skilldistillery.foodtruck.app;
 
-import com.skilldistillery.foodtruck.entities.FoodTruck; 
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+import java.util.Scanner;
 
-public class FoodTruckApp {	
-	private FoodTruck[] fleet =  new FoodTruck[5];
-	
+public class FoodTruckApp {
+	private FoodTruck[] fleet = new FoodTruck[5];
+	private Scanner kb = new Scanner(System.in);
+
 	public static void main(String[] args) {
-FoodTruckApp fta = new FoodTruckApp();
+		FoodTruckApp fta = new FoodTruckApp();
 		fta.run();
-		
+
 	}
+
 	public void run() {
-		// prompt user 
-		//write a menu for user input
-		
+		userInput();
+		menu2();
+
+		kb.close();
+		// write a menu for user input
 	}
-	// hold the 
+
+	public void userInput() {
+		for (int i = 0; i < fleet.length; i++) {
+			System.out.print("Enter the name of the food truck" + (i + 1) + " : ");
+			String truckName = kb.nextLine();
+
+			if (truckName.equals("quit")) { // do i have this correct ?
+				System.out.println("Have a good day.");
+
+				break; /// or break;
+			}
+			System.out.print("What type of food did you eat? :  ");
+			String foodType = kb.nextLine();
+			System.out.print("Rate your overall experience from 1 - 5:  ");
+			int rating = kb.nextInt();
+			FoodTruck newTruck = new FoodTruck(truckName, foodType, rating);
+			kb.nextLine();
+
+			fleet[i] = newTruck;
+
+		}
+
+	}
+
+	public void menu2() {
+		boolean status = true;
+		while (status) {
+			System.out.println("choose from the following ");
+			System.out.println("1) List all Food Trucks  ");
+			System.out.println("2) Average rating of trucks ");
+			System.out.println("3) Highest rated trucks ");
+			System.out.println("4) Quit");
+			int userChoice = kb.nextInt();
+			kb.nextLine();
+			switch (userChoice) {
+			case 1:
+				for (FoodTruck foodTruck : fleet) {
+					if (foodTruck != null) {
+						System.out.println(foodTruck);
+
+					}
+
+				}
+				break;
+
+			case 2:
+				double ave = 0;
+				int counter = 0;
+
+				for (FoodTruck foodTruck : fleet) {
+					if (foodTruck != null) {
+						ave += foodTruck.getRating();
+						counter++;
+
+					}
+
+				}
+				ave = ave / counter;
+				System.out.println(ave);
+
+			case 3:
+				FoodTruck bestRated = null;
+				double currentHigh = 0.0;
+				for (FoodTruck foodTruck : fleet) {
+					if (foodTruck != null) {
+						if (foodTruck.getRating() > currentHigh) {
+							currentHigh = foodTruck.getRating();
+							bestRated = foodTruck;
+
+						}
+
+					}
+
+				}
+				System.out.println(bestRated);
+				break;
+			case 4:
+				System.out.println("Enjoy your day");
+				status = false;
+				break;
+
+			default:
+				System.out.println("Invaid selection");
+				break;
+
+			}
+		}
+	}
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,8 +1,107 @@
 package com.skilldistillery.foodtruck.entities;
 
-public class FoodTruck {
-	
-private static int nextTruckId;
+import java.util.Objects;
 
+public class FoodTruck {
+
+	private static int truckId = 1;
+	private int id;
+	private String foodTruckName;
+	private String foodType;
+	private int rating;
+	
+	public FoodTruck() {
+		
+	}
+	
+
+	public FoodTruck(String foodTruckName, String foodType, int rating) {
+		this.id = truckId++;
+		this.foodTruckName = foodTruckName;
+		this.foodType = foodType;
+		this.rating = rating;
+		
+
+	}
+
+
+	public static int getTruckId() {
+		return truckId;
+	}
+
+
+	public static void setTruckId(int truckId) {
+		FoodTruck.truckId = truckId;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFoodTruckName() {
+		return foodTruckName;
+	}
+
+
+	public void setFoodTruckName(String foodTruckName) {
+		this.foodTruckName = foodTruckName;
+	}
+
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+
+	public int getRating() {
+		return rating;
+	}
+
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(foodTruckName, foodType, id, rating);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FoodTruck other = (FoodTruck) obj;
+		return Objects.equals(foodTruckName, other.foodTruckName) && Objects.equals(foodType, other.foodType)
+				&& id == other.id && rating == other.rating;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FoodTruck ID: " + id + ",   TruckName: " + foodTruckName + ",  Food Type: " + foodType + ",  Rating: "
+				+ rating;
+	}
+	
+	
+	
 
 }
